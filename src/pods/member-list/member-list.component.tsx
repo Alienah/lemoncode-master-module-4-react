5795d4c37b63ca04fac2d5866380852b26dcb174
Aida Albarrán Blanco <aalbarranbla@aalbarranbl-a01.vmware.com>
2021-08-21 16:42:54 +0200

    [feature] Add material ui and the new table

--- src/pods/member-list/member-list.component.tsx
+++ src/pods/member-list/member-list.component.tsx
@@ -1,43 +1,70 @@
 import React from 'react';
-import { Member } from './member-list.vm';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import { Link } from 'react-router-dom';
 import { routes } from 'core/router/routes';
+import { Member } from './member-list.vm';
 
 interface Props {
   members: Member[];
 }
 
+const useStyles = makeStyles({
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  root: {
+    width: '760px',
+    margin: 'auto',
+  },
+  avatarImg: {
+    width: '5rem',
+  },
+});
+
 export const MemberListComponent: React.FC<Props> = props => {
   const { members } = props;
+  const classes = useStyles();
+
   return (
-    <>
-      <h2>Hello from List page</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Avatar</th>
-            <th>Id</th>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {members.map(member => (
-            <tr key={member.id}>
-              <td>
-                <img src={member.avatarUrl} style={{ width: '5rem' }} />
-              </td>
-              <td>
-                <span>{member.id}</span>
-              </td>
-              <td>
-                <Link to={routes.memberDetail(member.login)}>
-                  {member.login}
-                </Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
+    <div className={classes.wrapper}>
+      <h2>Members list</h2>
+      <Paper className={classes.root}>
+        <TableContainer>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Avatar</TableCell>
+                <TableCell align="right">Id</TableCell>
+                <TableCell align="right">User</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {members.map(member => (
+                <TableRow key={member.id}>
+                  <TableCell>
+                    <img src={member.avatarUrl} className={classes.avatarImg} />
+                  </TableCell>
+                  <TableCell align="right">{member.id}</TableCell>
+                  <TableCell align="right">
+                    <Link to={routes.memberDetail(member.login)}>
+                      {member.login}
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </div>
   );
 };
